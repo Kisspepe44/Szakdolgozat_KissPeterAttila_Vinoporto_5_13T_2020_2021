5d5c6a90e00ba9572eb69b45eac2ea744535b7b9
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-12-29 15:37:11 +0100

    Kezdolap 2 gomb es egy proba logo

--- Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021/Form1.Designer.cs
+++ Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021/Form1.Designer.cs
@@ -29,27 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kezdolap));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.kl_kikep_btn = new System.Windows.Forms.Button();
+            this.kl_tovabb_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // kl_kikep_btn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 578);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(252, 75);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.kl_kikep_btn.BackColor = System.Drawing.Color.DarkRed;
+            this.kl_kikep_btn.Font = new System.Drawing.Font("Script MT Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kl_kikep_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kl_kikep_btn.Location = new System.Drawing.Point(12, 578);
+            this.kl_kikep_btn.Name = "kl_kikep_btn";
+            this.kl_kikep_btn.Size = new System.Drawing.Size(250, 75);
+            this.kl_kikep_btn.TabIndex = 0;
+            this.kl_kikep_btn.Text = "Kilépés!";
+            this.kl_kikep_btn.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // kl_tovabb_btn
             // 
-            this.button2.Location = new System.Drawing.Point(998, 578);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(252, 75);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.kl_tovabb_btn.BackColor = System.Drawing.Color.DarkRed;
+            this.kl_tovabb_btn.Font = new System.Drawing.Font("Script MT Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kl_tovabb_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kl_tovabb_btn.Location = new System.Drawing.Point(998, 578);
+            this.kl_tovabb_btn.Name = "kl_tovabb_btn";
+            this.kl_tovabb_btn.Size = new System.Drawing.Size(250, 75);
+            this.kl_tovabb_btn.TabIndex = 1;
+            this.kl_tovabb_btn.Text = "Tovább!";
+            this.kl_tovabb_btn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(475, 238);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Kezdolap
             // 
@@ -57,18 +75,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.kl_tovabb_btn);
+            this.Controls.Add(this.kl_kikep_btn);
             this.Name = "Kezdolap";
             this.Text = "Kezdőlap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button kl_kikep_btn;
+        private System.Windows.Forms.Button kl_tovabb_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
