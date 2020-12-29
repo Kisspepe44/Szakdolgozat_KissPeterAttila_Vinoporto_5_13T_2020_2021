52332f508c8576c194a9b460f23882f34db7ac18
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-12-29 15:23:24 +0100

    ujrameretezes kezdolap 1280-720

--- Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021/Form1.Designer.cs
+++ Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021/Form1.Designer.cs
@@ -29,14 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kezdolap));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 578);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(998, 578);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Kezdolap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1269, 688);
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Kezdolap";
             this.Text = "Kezdőlap";
             this.ResumeLayout(false);
@@ -44,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
