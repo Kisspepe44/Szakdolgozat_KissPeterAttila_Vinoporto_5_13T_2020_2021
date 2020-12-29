ef6dd1f5603a04244f89410433dcfbbaba38c747
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-12-29 15:13:11 +0100

    Add project files.

---
+++ Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szakdolgozat_KissPeterAttila_Vinoporto_5_13T_2020_2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
